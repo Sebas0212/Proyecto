9746cb317397e35682eb8ca918f578715b6568c6
Sebas0212 <sebas.hdez.02.sh@gmail.com>
2024-11-22 15:21:17 -0600

    Add files via upload

---
+++ ProyectoProgra/src/proyectoprogra/capaLogica/Cine.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoprogra.capaLogica;
+
+/**
+ *
+ * @author sebas
+ */
+public class Cine {
+    private String [] Pelicula;
+    private String [][] sala1 = new String [10] [8];
+    private String [][] Sala2 = new String [10] [8];
+    private String [][] Sala3 = new String [10] [8];
+    
+    public void prueba(){
+     
+    }
+}
